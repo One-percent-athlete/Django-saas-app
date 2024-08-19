1697c6e181b89da1d35a57037b166d59daba75d7
One-percent-athlete <one.percent.athlete@gmail.com>
2024-08-19 19:31:32 +0900

    Bug fix

--- src/home/settings.py
+++ src/home/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-from decouple import config
+from decouple import config as decouple_config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,11 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('DJANGO_SECRET_KEY')
+SECRET_KEY = decouple_config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = str(os.environ.get('DEBUG')).lower() == 'true'
-DEBUG = config('DJANGO_DEBUG', cast=bool)
+DEBUG = decouple_config('DJANGO_DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['.railway.app']
 
@@ -90,18 +90,19 @@ DATABASES = {
 }
 
 
-CONN_MAX_AGE = config('CONN_MAX_AGE', cast=int, default=30)
-DATABASE_URL = config('DATABASE_URL', cast=str)
+CONN_MAX_AGE = decouple_config('CONN_MAX_AGE', cast=int, default=30)
+DATABASE_URL = decouple_config('DATABASE_URL', cast=str)
 
 if DATABASE_URL is not None:
     import dj_database_url
     DATABASES = {
         'default': dj_database_url.config(
-            default = config('DATABASE_URL', cast=str),
+            default=DATABASE_URL,
             conn_max_age=CONN_MAX_AGE,
-            conn_health_checks=True,
+            conn_health_checks=True
         )
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
