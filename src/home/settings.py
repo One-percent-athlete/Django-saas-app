4e8cc9c5f7e9b32b1da8ef57534c53b709862c14
One-percent-athlete <one.percent.athlete@gmail.com>
2024-08-19 17:56:43 +0900

    Add python decouple

--- src/home/settings.py
+++ src/home/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mm6voz^@b(&_tbj^5%#p_hf_6ebfl#*uo@+mzs-^9gsxu3a23s'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = str(os.environ.get('DEBUG')).lower() == 'true'
+DEBUG = config('DJANGO_DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['.railway.app']
 
