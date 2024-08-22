37307016b569ed5a9b38e4a7bed8e6e6569f0d4f
One-percent-athlete <one.percent.athlete@gmail.com>
2024-08-23 03:14:13 +0900

    Add user pnly page using django decoretors

--- src/home/urls.py
+++ src/home/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('protected/', views.pw_protected_view, name='protedted'),
+    path('protected/user_only/', views.user_only_view, name='user_only'),
 
     path('', views.home_view, name='home'),
     path('about/', views.about_view, name='about'),
