2ccacce60334792bd1fd3a1c2b7c94aa0f08d5f9
One-percent-athlete <one.percent.athlete@gmail.com>
2024-08-26 01:19:11 +0900

    Add custom group feature

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -4,6 +4,7 @@ from django.db.models.signals import post_save
 from django.conf import settings
 
 User = settings.AUTH_USER_MODEL
+ALLOW_CUSTOM_GROUPS = True
 
 SUB_PERMS = [
         ("free", "Free Perm"),
@@ -44,6 +45,18 @@ def user_sub_post_save(sender, instance, *args, **kwargs):
     user = user_sub_instance.user
     subscription_obj = user_sub_instance.subscription
     groups = subscription_obj.groups.all()
-    user.groups.set(groups)
+    if not ALLOW_CUSTOM_GROUPS:
+        user.groups.set(groups)
+    else:
+        subs_qs = Subscription.objects.filter(active=True)
+        subs_groups = subs_qs.values_list('gourps__id', flat=True).exclude(id=subscription_obj.id)
+        subs_groups_set = set(subs_groups)
+
+        groups_ids = groups.values_list('id', flat=True)
+        current_groups = user.groups.all().values_list('id', flat=True)
+        groups_ids_set = set(groups_ids)
+        current_groups_set = set(current_groups) - subs_groups_set
+        final_groups_ids = list(groups_ids_set | current_groups_set)
+        user.groups.set(final_groups_ids)
 
 post_save.connect(user_sub_post_save, sender=UserSubscription)
