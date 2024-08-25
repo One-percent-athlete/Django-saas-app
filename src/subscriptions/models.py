a37c1f95dbdb7b2b8d2e55ae0d6c4c31417998e5
One-percent-athlete <one.percent.athlete@gmail.com>
2024-08-26 01:23:05 +0900

    Fix bug

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -44,15 +44,19 @@ def user_sub_post_save(sender, instance, *args, **kwargs):
     user_sub_instance = instance
     user = user_sub_instance.user
     subscription_obj = user_sub_instance.subscription
-    groups = subscription_obj.groups.all()
+    groups_ids = []
+    if subscription_obj is not None:
+        groups = subscription_obj.groups.all()
+        groups_ids = groups.values_list('id', flat=True)
     if not ALLOW_CUSTOM_GROUPS:
         user.groups.set(groups)
     else:
         subs_qs = Subscription.objects.filter(active=True)
-        subs_groups = subs_qs.values_list('gourps__id', flat=True).exclude(id=subscription_obj.id)
+        if subscription_obj is not None:
+            subs_qs = subs_qs.exclude(id=subscription_obj.id)
+        subs_groups = subs_qs.values_list('gourps__id', flat=True)
         subs_groups_set = set(subs_groups)
 
-        groups_ids = groups.values_list('id', flat=True)
         current_groups = user.groups.all().values_list('id', flat=True)
         groups_ids_set = set(groups_ids)
         current_groups_set = set(current_groups) - subs_groups_set
