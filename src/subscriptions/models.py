e592704d5acc218c505a557b9830af2da49f4ea0
One-percent-athlete <one.percent.athlete@gmail.com>
2024-08-28 23:37:37 +0900

    Add subscription price model and manage price with stripe

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -17,7 +17,6 @@ SUB_PERMS = [
 
 
 class Subscription(models.Model):
-
     name = models.CharField(max_length=120)
     active = models.BooleanField(default=True)
     groups = models.ManyToManyField(Group)
@@ -38,11 +37,52 @@ class Subscription(models.Model):
     
     def save(self, *args, **kwargs):
         if not stripe_id:
-            stripe_id = billing.create_product(name=self.name, metadata={'subscription_plan__id': self.id}, raw=False)
+            stripe_id = billing.create_product(name=self.name, metadata={'subscription_plan_id': self.id}, raw=False)
             self.stripe_id = stripe_id
         
         super().save(*args, **kwargs)
+
+
+class SubscriptionPrice(models.Model):
+
+    class IntervalChoices(models.TextChoices):
+        MONTHLY = 'month', 'Monthly'
+        YEARLY = 'year', 'Yearly'
+
+    subscription = models.ForeignKey(Subscription, on_delete=models.SET_NULL, null=True)
+    stripe_id = models.CharField(max_length=120, null=True, blank=True)
+    interval = models.CharField(max_length=120, default=IntervalChoices.MONTHLY, choices=IntervalChoices.choices)
+
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=99.99)
+
+    @property
+    def stripe_currency(self):
+        return 'usd'
     
+    @property
+    def stripe_price(self):
+        return self.price * 100
+    
+    @property
+    def product_stripe_id(self):
+        if not self.subscription:
+            return None
+        return self.subscription.stripe_id
+    
+    def save(self, *args, **kwargs):
+
+        if (not self.stripe_id and self.product_stripe_id is not None):
+            stripe_id = billing.create_price(
+                currency=self.stripe_currency,
+                unit_amount=self.stripe_price,
+                interval=self.interval,
+                product=self.product_stripe_id,
+                metadata={'subscription_plan_price_id': self.id}, raw=False
+            )
+            self.stripe_id = stripe_id
+        super().save(*args, **kwargs)
+
+
 class UserSubscription(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     subscription = models.ForeignKey(Subscription, on_delete=models.SET_NULL, null=True, blank=True)
