708b252b78f395cdef7c59e6b2c912ae830162e1
One-percent-athlete <one.percent.athlete@gmail.com>
2024-09-06 21:18:03 +0900

    Create checkout vivews

--- src/checkouts/views.py
+++ src/checkouts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+def product_price_redirect_view(request, price_id=None, *args, **kwargs):
+    request.session['checkout_subscription_stripe_id'] = price_id
+    return render("/checkout")
+
+@login_required
+def checkout_redirect_view(request):
+    checkout_subscription_stripe_id = request.session.get("checkout_subscription_stripe_id")
+    
+    if checkout_subscription_stripe_id is None:
+        return redirect("/pricing")
+    customer_stripe_id = request.user.customer.stripe_id
+    return redirect("/checkout")
+
+@login_required
+def checkout_finalize_view(request):
+    return 
