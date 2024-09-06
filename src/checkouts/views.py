2acbd3ca066909a1173f7d81220fb4f0ef984e5e
One-percent-athlete <one.percent.athlete@gmail.com>
2024-09-06 21:33:33 +0900

    Update subscription price model and checkout view

--- src/checkouts/views.py
+++ src/checkouts/views.py
@@ -1,20 +1,25 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
+from subscriptions.models import SubscriptionPrice
 
 # Create your views here.
 def product_price_redirect_view(request, price_id=None, *args, **kwargs):
     request.session['checkout_subscription_stripe_id'] = price_id
-    return render("/checkout")
+    return redirect("checkout_start")
 
 @login_required
 def checkout_redirect_view(request):
     checkout_subscription_stripe_id = request.session.get("checkout_subscription_stripe_id")
-    
-    if checkout_subscription_stripe_id is None:
-        return redirect("/pricing")
+    try:
+        obj = SubscriptionPrice.objects.get(id=checkout_subscription_stripe_id)
+    except:
+        obj = None
+
+    if checkout_subscription_stripe_id is None or obj is None:
+        return redirect("pricing")
     customer_stripe_id = request.user.customer.stripe_id
-    return redirect("/checkout")
+    return redirect("/checkout/")
 
 @login_required
 def checkout_finalize_view(request):
--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import Group, Permission
 from django.db import models
 from django.db.models.signals import post_save
 from django.conf import settings
+from django.urls import reverse
 
 from helpers import billing
 
@@ -75,6 +76,9 @@ class SubscriptionPrice(models.Model):
 
     class Meta:
         ordering = ['subscription__order', 'order', 'featured', '-updated']
+
+    def get_checkout_url(self):
+        return reverse('sub_price_checkout', kwargs={'price_id': self.id})
 
     @property
     def display_features_list(self):
